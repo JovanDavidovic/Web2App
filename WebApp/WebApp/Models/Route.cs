1a26e5a6e15f8cec143d1ccfdd8eef40f4f8b01c
JovanDavidovic <jovandavidovic96@gmail.com>
2019-05-28 13:20:37 +0200

    Cenovnik, CenaKarta, Ruta

---
+++ WebApp/WebApp/Models/Route.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Route
+    {
+        [Key]
+        public int Number { get; set; }
+        public List<Station> Stations { get; set; }
+        public List<Bus> Buses { get; set; }
+    }
+}
---
+++ WebApp/WebApp/Models/TicketPrice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class TicketPrice
+    {
+        public int Id { get; set; }
+        public int Cena { get; set; }
+        //Mozda treba foreignKey
+        public int PricelistId { get; set; }
+        public Pricelist Pricelist { get; set; }
+        public int TicketTypeId { get; set; }
+        public TicketType TicketType { get; set; }
+    }
+}
