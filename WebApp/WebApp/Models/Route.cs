bd448fc2f4a948a6b1643c937191043cdeb68fe2
JovanDavidovic <jovandavidovic96@gmail.com>
2019-05-30 13:05:07 +0200

    DepartureTime, Ticket, Route changed

--- WebApp/WebApp/Models/Route.cs
+++ WebApp/WebApp/Models/Route.cs
@@ -12,5 +12,11 @@ namespace WebApp.Models
         public int Number { get; set; }
         public List<Station> Stations { get; set; }
         public List<Bus> Buses { get; set; }
+
+        public int DepartureTimeId { get; set; }
+        public DepartureTime DepartureTime { get; set; }
+
+        public int DayTypeId { get; set; }
+        public DayType DayType { get; set; }
     }
 }
---
+++ WebApp/WebApp/Models/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public int TicketPriceId { get; set; }
+        public TicketPrice TicketPrice { get; set; }
+
+        public int PassengerId { get; set; }
+        public Passenger Passenger { get; set; }
+
+        public int TotalPrice { get; set; }
+    }
+}
