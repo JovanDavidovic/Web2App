a6f57c80c34b526f95fd2f34067b3c45aa11825c
dunjabursac <dunja.bursac@hotmail.com>
2019-06-01 14:00:54 +0200

    1/2 Repository

---
+++ WebApp/WebApp/Persistence/Repository/TicketTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Persistence.Repository
+{
+    public class TicketTypeRepository : Repository<TicketTypeRepository, int>, ITicketTypeRepository
+    {
+        public TicketTypeRepository(DbContext context) : base(context)
+        { }
+    }
+}
